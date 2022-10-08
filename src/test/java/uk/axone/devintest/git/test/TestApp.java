02d02c7204c652d48f32a76b8e01ce708a9e761c
PateRom <romalpesh@gmail.com>
2022-10-08 16:41:32 +0100

    Jira 123- committing new project first time

---
+++ src/test/java/uk/axone/devintest/git/test/TestApp.java
@@ -0,0 +1,10 @@
+package uk.axone.devintest.git.test;
+
+import org.junit.Test;
+
+public class TestApp {
+    @Test
+    public void test1(){
+        System.out.println("running test 1");
+    }
+}
